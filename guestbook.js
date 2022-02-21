9502265837c499436ff52fc36683f810f18653b8
pauloandrade-pt <75178060+pauloandrade-pt@users.noreply.github.com>
2022-02-21 20:09:18 +0000

    Update guestbook.js
    Added API url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f924ec65.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
